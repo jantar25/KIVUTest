b09e110430eb2419a0d2bae41ec9d20a6d4f30d8
unknown <glodybulambo@gmail.com>
2021-07-16 20:07:43 +0200

    adding web interaction
    javascript interaction

--- javascript.js
+++ javascript.js
@@ -1,9 +1,55 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLists = document.querySelector(".navbar-menu");
+const navLogo = document.querySelector("#navbar-logo");
 
+//display mobile Menu
 const mobileMenu = () => {
   menu.classList.toggle("is-active");
   menuLists.classList.toggle("active");
 };
-
 menu.addEventListener("click", mobileMenu);
+
+//Hiding Mobile Menu
+const hideMobMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLists.classList.remove("active");
+  }
+};
+menuLists.addEventListener("click", hideMobMenu);
+navLogo.addEventListener("click", hideMobMenu);
+
+//Adding Highlights on Menu
+
+const highlightMenu = () => {
+  const highlightElem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#About-page");
+  const specialitiesMenu = document.querySelector("#Specialities-page");
+  let scrollPosition = window.scrollY;
+  console.log(scrollPosition);
+
+  if (window.innerWidth > 960 && scrollPosition < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPosition < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    specialitiesMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPosition < 2345) {
+    specialitiesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  }
+  if (
+    (highlightElem && window.innerWidth < 960 && scrollPosition < 600) ||
+    highlightElem
+  ) {
+    highlightElem.classList.remove("highlight");
+  }
+};
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
